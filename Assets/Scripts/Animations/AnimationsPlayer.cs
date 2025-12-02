3160874c3b1df21ff0097cca575d0228f2968e84
victor176S <hdvictor176@gmail.com>
2025-12-02 11:44:22 +0100

    Animation setup + Jump + Still + Falling animations

---
+++ Assets/Scripts/Animations/AnimationsPlayer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class AnimationsPlayer : MonoBehaviour
+{
+
+    public static AnimationsPlayer instance;
+
+    [SerializeField] private Animator animator;
+
+    private bool cargandoSalto;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (PlayerMovement.instance.botonSaltoMantenido && PlayerMovement.instance.enSuelo)
+        {
+            cargandoSalto = true;
+        }
+
+        else
+        {
+            cargandoSalto = false;
+        }
+
+        animator.SetFloat("X", GameManager.instance.player.GetComponent<PlayerMovement>().rb.linearVelocityX);
+
+        animator.SetFloat("Y", GameManager.instance.player.GetComponent<PlayerMovement>().rb.linearVelocityY);
+
+        animator.SetTrigger("Saltar");
+
+        animator.SetBool("enSuelo", PlayerMovement.instance.enSuelo);
+
+        animator.SetBool("OnDash", PlayerDash.instance.dashEnCurso);
+
+        animator.SetBool("CargandoSalto", cargandoSalto);
+        
+    }
+}
